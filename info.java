f65e32fcfcf00f058200e7658338490eee8b37a2
nabilahfarahin <nabilahfarahin560@gmail.com>
2018-03-17 12:15:08 +0800

    Add existing file

---
+++ info.java
@@ -0,0 +1,37 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
+
+public class info
+{
+	public static void main(String args[])
+	{
+	
+		Socket conn = null;
+
+		try{   
+
+			InetAddress ipAddr = InetAddress.getLocalHost();
+			System.out.println(ipAddr.getHostAddress());
+			String hostname = ipAddr.getHostName();
+			System.out.println("Hostname of system is: " 
++hostname);
+		while(true)
+		{
+		
+		echo("Connection received from " 
++conn.getInetAddress().getHostName() +" : " +conn.getPort());
+		
+		}
+
+		   }
+
+		catch(UnknownHostException e) {
+		System.err.println("IOException");  }
+		
+	
+	}
+		public static void echo(String msg) {
+		System.out.println(msg);  }
+}
